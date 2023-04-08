19a58a965581fe85a57fdb4f023d117a592560d7
ChrisCvid <129689313+ChrisCvid@users.noreply.github.com>
2023-04-08 14:51:04 -0400

    Taller 1 con mejoras

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,80 @@
 import edu.princeton.cs.stdlib.StdDraw;
-
 import java.awt.*;
-
+/**
+ * Dibujar 1 linea que rebote en la pantalla junto a otras 5 lineas que imiten su comportamiento
+ *
+ * @author: Christopher Cortes
+ * */
 public class Main {
+    /**
+    * Main
+    */
     public static void main(String[] args) {
-        double[][] Lineas = new double[ 2 ][ 4 ];
-        double min = -1.0;   //se asignan variables, destinadas a escalar el lienzo
+        double[] OrigenLinea = new double[ 4 ];
+        double[] desplazamiento = new double[ 4 ];
+
+        //rango del lienzo
+        double min = -1.0;
         double max = 1.0;
 
-        StdDraw.setXscale (min, max);
-        StdDraw.setYscale (min, max); //se escala el lienzo usando las variables previamente definidas
+        //escala del lienzo
+        StdDraw.setXscale (min , max);
+        StdDraw.setYscale (min , max);
 
-        DatosLinea (Lineas, min, max);
+        DatosLinea (OrigenLinea , desplazamiento , min , max);
 
-        StdDraw.enableDoubleBuffering (); // esta funcion evita el parpadeo de las lineas
+        //evitar el parpadeo de las lineas
+        StdDraw.enableDoubleBuffering ();
 
+        Funcionamiento (OrigenLinea , desplazamiento);
+    }
+
+    private static void Funcionamiento(double[] OrigenLinea , double[] desplazamiento) {
+        //ciclo infinito
         while (true) {
-
-            desplazamiento (Lineas);
-            genLineas (Lineas[ 1 ][ 0 ], Lineas[ 1 ][ 1 ], Lineas[ 1 ][ 2 ], Lineas[ 1 ][ 3 ]);//generacion de las 6 lineas, se definen colores para cada una en relacion a los colores del arcoiris
-            StdDraw.pause ((int) Math.random ());    //
+            MovLinea (OrigenLinea , desplazamiento);
+            GenLinea (OrigenLinea);
             StdDraw.show ();
-
-            StdDraw.clear (Color.BLACK);                                         //se define el fondo de color negro y elimina las lineas anteriores
+            StdDraw.clear (Color.BLACK);
 
         }
-    }
+    }/**
+     *funcion que usa todos los datos declarados para mostrar el movimiento de las lineas generadas
+     **/
 
-    private static void DatosLinea(double[][] Lineas, double min, double max) {
-        Lineas[ 0 ][ 0 ] = (0.01) * Math.random ();
-        Lineas[ 0 ][ 1 ] = (0.01) * Math.random ();
-        Lineas[ 0 ][ 2 ] = (0.01) * Math.random ();
-        Lineas[ 0 ][ 3 ] = (0.01) * Math.random ();
-
-        Lineas[ 1 ][ 0 ] = min + (max - min) * Math.random ();
-        Lineas[ 1 ][ 1 ] = min + (max - min) * Math.random ();
-
-        Lineas[ 1 ][ 2 ] = min + (max - min) * Math.random ();
-        Lineas[ 1 ][ 3 ] = min + (max - min) * Math.random ();
-    } // operaciones para generar los datos iniciales de la linea 1 (velocidad y punto de inicio)
-
-    private static void desplazamiento(double[][] Lineas) {
-        if (Math.abs (Lineas[ 1 ][ 0 ] + Lineas[ 0 ][ 0 ]) > 1) {
-            Lineas[ 0 ][ 0 ] = -Lineas[ 0 ][ 0 ];
+    private static void MovLinea(double[] OrigenLinea , double[] desplazamiento) {
+        //colision
+        for (int i = 0 ; i < 4 ; i++) {
+            if (Math.abs (OrigenLinea[ i ] + desplazamiento[ i ]) > 1) {
+                desplazamiento[ i ] = -desplazamiento[ i ];
+                OrigenLinea[ i ] += desplazamiento[ i ];
+            }
         }
-        if (Math.abs (Lineas[ 1 ][ 1 ] + Lineas[ 0 ][ 1 ]) > 1) {
-            Lineas[ 0 ][ 1 ] = -Lineas[ 0 ][ 1 ];
+        //movimiento de la linea
+        for (int c = 0 ; c < 4 ; c++) {
+            OrigenLinea[ c ] += desplazamiento[ c ];
         }
-        if (Math.abs (Lineas[ 1 ][ 2 ] + Lineas[ 0 ][ 0 ]) > 1) {
-            Lineas[ 0 ][ 2 ] = -Lineas[ 0 ][ 2 ];
+    }/**
+     *Funcion con las operaciones matematicas ligadas al movimiento de la linea dentro del lienzo
+     **/
+    private static void DatosLinea(double[] OrigenLinea , double[] desplazamiento , double min , double max) {
+        for (int i = 0 ; i < 4 ; i++) {
+            desplazamiento[ i ] = (0.01) * Math.random ();
+            OrigenLinea[ i ] = min + (max - min) * Math.random ();
         }
-        if (Math.abs (Lineas[ 1 ][ 3 ] + Lineas[ 0 ][ 1 ]) > 1) {
-            Lineas[ 0 ][ 3 ] = -Lineas[ 0 ][ 3 ];
+    }/**
+     *Funcion con las operaciones matematicas ligadas al punto de origen de la linea y la velocidad de desplazamiento
+     **/
+    private static void GenLinea(double[] origenLinea) {
+        double pos=0;
+        Color[] colores = {Color.RED , Color.ORANGE , Color.YELLOW , Color.GREEN , Color.BLUE , Color.MAGENTA};
+        //ciclo finito que genera lineas
+        for (int i = 0 ; i < 6 ; i++) {
+            StdDraw.setPenColor (colores[ i ]);
+            StdDraw.line (origenLinea[ 0 ] + pos , origenLinea[ 1 ] + pos , origenLinea[ 2 ] + pos , origenLinea[ 3 ] + pos);
+            pos -= 0.02;
         }
-
-        Lineas[ 1 ][ 0 ] += Lineas[ 0 ][ 0 ];
-        Lineas[ 1 ][ 1 ] += Lineas[ 0 ][ 1 ];
-        Lineas[ 1 ][ 3 ] += Lineas[ 0 ][ 3 ];
-        Lineas[ 1 ][ 2 ] += Lineas[ 0 ][ 2 ];
-    } // operaciones para limitar el tipo de movimiento que tendra la linea 1
-
-    private static void genLineas(double x0, double y0, double x1, double y1) {
-        StdDraw.setPenColor (StdDraw.RED);
-        StdDraw.line (x0, y0, x1, y1);
-        StdDraw.setPenColor (StdDraw.ORANGE);
-        StdDraw.line (x0 - 0.02, y0 - 0.02, x1 - 0.02, y1 - 0.02);
-        StdDraw.setPenColor (StdDraw.YELLOW);
-        StdDraw.line (x0 - 0.04, y0 - 0.04, x1 - 0.04, y1 - 0.04);
-        StdDraw.setPenColor (StdDraw.GREEN);
-        StdDraw.line (x0 - 0.06, y0 - 0.06, x1 - 0.06, y1 - 0.06);
-        StdDraw.setPenColor (StdDraw.BLUE);
-        StdDraw.line (x0 - 0.08, y0 - 0.08, x1 - 0.08, y1 - 0.08);
-        StdDraw.setPenColor (StdDraw.MAGENTA);
-        StdDraw.line (x0 - 0.10, y0 - 0.10, x1 - 0.10, y1 - 0.10);
-    }
+    }/**
+     *Funcion destinada a la generacion de lineas en el lienzo
+     **/
 }
